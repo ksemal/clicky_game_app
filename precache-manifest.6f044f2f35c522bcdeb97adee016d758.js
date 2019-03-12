39d018abe958535d5fc7534c754f78b4ea5a4d64
km <42688915+ksemal@users.noreply.github.com>
2019-03-11 21:21:31 -0400

    Updates

---
+++ precache-manifest.6f044f2f35c522bcdeb97adee016d758.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "8731872251c4fb31822b",
+    "url": "/clicky_game_app/static/js/runtime~main.8ee0a12d.js"
+  },
+  {
+    "revision": "dcee1dcf5804ed92d723",
+    "url": "/clicky_game_app/static/js/main.25e2d8c8.chunk.js"
+  },
+  {
+    "revision": "ff661de6195320cfc0eb",
+    "url": "/clicky_game_app/static/js/2.822769df.chunk.js"
+  },
+  {
+    "revision": "dcee1dcf5804ed92d723",
+    "url": "/clicky_game_app/static/css/main.53bda4b8.chunk.css"
+  },
+  {
+    "revision": "defd2df07ec55a714e9110f6fd18d57f",
+    "url": "/clicky_game_app/index.html"
+  }
+];
