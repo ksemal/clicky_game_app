7e6483d7b3ac0e58e2cd4b153f5c7da5590b2fa3
km <42688915+ksemal@users.noreply.github.com>
2019-03-11 21:04:35 -0400

    set up new repo

---
+++ README.md
@@ -0,0 +1,20 @@
+## Clicky Game
+
+### Overview
+
+A memory game with React.
+
+- The application render different images to the screen. Each image listens for click events.
+
+- The application keeps track of the user's score. The user's score increments when clicking an image for the first time. The user's score resets to 0 if they click the same image more than once.
+
+- Every time an image is clicked, the images rendered to the page shuffle themselves in a random order.
+
+- Once the user's score is reset after an incorrect guess, the game restarts.
+
+## Tech:
+
+- [React]
+- [Bootstrap]
+
+![project screenshots](public/img/screencapture.png)
---
+++ src/components/Wrapper/index.js
@@ -0,0 +1,34 @@
+import React from "react";
+import "./style.css";
+
+function Wrapper(props) {
+  return (
+    <div className="container-fluid">
+      <nav class="navbar fixed-top navbar-light bg-light">
+        <a className="title" href="/">
+          <i class="fas fa-dice" />
+          &nbsp; Clicky Game
+        </a>
+        <div className={props.class} onAnimationEnd={props.anim}>
+          {props.text}
+        </div>
+        <div>
+          <span>Score: {props.score}</span> |
+          <span> Top score: {props.topScore}</span>
+        </div>
+      </nav>
+      <div className="bg">
+        <h2>Clicky Game!</h2> Click on an image to earn points, but don't click
+        on any more than once!
+      </div>
+      <div className={`wrapper ${props.class}`}>{props.children}</div>
+      <div className="footer">
+        <a href="https://github.com/ksemal/Clicky-Game">
+          <i class="fab fa-github-square" /> KM
+        </a>
+      </div>
+    </div>
+  );
+}
+
+export default Wrapper;
