16cb6b743ba79e0f13c36e850dfbf165c49e40e3
km <42688915+ksemal@users.noreply.github.com>
2019-03-13 21:55:07 -0400

    changed css

--- src/components/Wrapper/index.js
+++ src/components/Wrapper/index.js
@@ -3,31 +3,33 @@ import "./style.css";
 
 function Wrapper(props) {
   return (
-    <div className="container-fluid">
-      <nav class="navbar fixed-top navbar-light bg-light">
-        <a className="title" href="/">
-          <i className="fas fa-dice" />
-          &nbsp; Clicky Game
-        </a>
-        <div className={props.class} onAnimationEnd={props.anim}>
-          {props.text}
+    <>
+      <div className="container-fluid">
+        <nav class="navbar fixed-top navbar-light bg-light">
+          <a className="title" href="/">
+            <i className="fas fa-dice" />
+            &nbsp; Clicky Game
+          </a>
+          <div className={props.class} onAnimationEnd={props.anim}>
+            {props.text}
+          </div>
+          <div>
+            <span>Score: {props.score}</span> |
+            <span> Top score: {props.topScore}</span>
+          </div>
+        </nav>
+        <div className="bg">
+          <h2>Clicky Game!</h2> Click on an image to earn points, but don't
+          click on any more than once!
         </div>
-        <div>
-          <span>Score: {props.score}</span> |
-          <span> Top score: {props.topScore}</span>
-        </div>
-      </nav>
-      <div className="bg">
-        <h2>Clicky Game!</h2> Click on an image to earn points, but don't click
-        on any more than once!
+        <div className={`wrapper ${props.class}`}>{props.children}</div>
       </div>
-      <div className={`wrapper ${props.class}`}>{props.children}</div>
       <div className="footer">
         <a href="https://github.com/ksemal/Clicky-Game">
           <i className="fab fa-github-square" /> KM
         </a>
       </div>
-    </div>
+    </>
   );
 }
 
